9eb5c656609cfd254c6db2d67d8ab4fc4334ec50
Priya Bathija <priyab99@hotmail.com>
2018-11-04 21:28:05 -0500

    done css for finduser

--- finduser.js
+++ finduser.js
@@ -2,6 +2,8 @@
 const users = [] // Array of books owned by the library (whether they are loaned or not)
 const userSearchForm = document.querySelector('#userSearchForm');
 
+
+
 class User {
 	constructor(name, email, active) {
 		this.name = name;
@@ -14,24 +16,48 @@ users.push(new User('Andriy', 'andriy123@gmail.com', 'Jan 1, 2018'));
 users.push(new User('Brennan', 'brennan@gmail.com', 'Jan 2, 2018'));
 users.push(new User('Priya', 'priya123@gmail.com', 'Jan 3, 2018'));
 users.push(new User('Jerry', 'jerry123@gmail.com', 'Jan 4, 2018'));
+users.push(new User('Priya2', 'priya222@gmail.com', 'Jan 5, 2018'));
 
-userSearchForm.addEventListener('submit', searchUser);
+userSearchForm.addEventListener('input', searchUser);
 
 
 function searchUser(e) {
 	e.preventDefault();
+	
+	document.getElementById('allDisplays').innerHTML = "";
 	let email = userSearchForm.querySelector('#userEmail').value;
+	if(email == ""){
+		document.getElementById('allDisplays').innerHTML = "";
+		return;
+	}
 	for(var i = 0; i < users.length; i++){
-		if(email == users[i].email){
+		if(users[i].email.includes(email)){
 			displayUser(users[i]);
 		}
 	}
 }
 
 function displayUser(user){
-	let display = document.getElementById('userDisplay').getElementsByTagName('span');
+	let markup = `
+	<div id = "innerWrapper">
+		<div id = "userDisplay" class = "user-entry">
+			<div id = "userInfo">
+				<p><b>Name:</b> ${user.name}</p>
+				<p><b>Email:</b> ${user.email}</p>
+				<p><b>Last Active:</b> ${user.active}</p>
+			</div>
+			<div id = "buttons">
+				<div id = "innerWrapper">
+					<button class = "button">Edit</button>
+				</div>
+				<div id = "innerWrapper">
+					<button class = "button">Delete</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	`;
 	
-	display[0].innerHTML = "Name: ".concat(user.name);
-	display[1].innerHTML = "Email: ".concat(user.email);
-	display[2].innerHTML = "Last Active: ".concat(user.active);
+	let display = document.getElementById('allDisplays');
+	display.innerHTML += markup;
 }
