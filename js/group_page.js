c8f9935345d6c5e61f0b028557e1b9e08efb9a17
Andriy <andriydzikh@gmail.com>
2018-11-08 14:29:34 -0500

    Redid the group page to look more like the assignment page.

---
+++ js/group_page.js
@@ -0,0 +1,116 @@
+
+const groupRequests = [
+    {name: "Hip Hippos", groupId: Math.floor(Math.random() * 1000000), numMembers: 2},
+    {name: "Peckish Penguins", groupId: Math.floor(Math.random() * 1000000), numMembers: 1},
+    {name: "Sinewy Centaurs", groupId: Math.floor(Math.random() * 1000000), numMembers: 5},
+    {name: "Diligent Dingos", groupId: Math.floor(Math.random() * 1000000), numMembers: 4},
+    {name: "Witty Walruses", groupId: Math.floor(Math.random() * 1000000), numMembers: 3},
+    {name: "Sagacious Squids", groupId: Math.floor(Math.random() * 1000000), numMembers: 1}
+]
+
+const user = "Andriy";
+const owner = "Brennan";
+
+const userGroup = {
+    name: "Bullish Frogs",
+    numMembers: 4,
+    members: ["Priya", "Jerry", "Andriy", "Brennan"],
+    requirements: ["Available Mon/Wed/Fri mornings", "Willing to work 8 hours/week"],
+    availability: {
+        0: [['09:00', '11:00'], ['13:00', '16:00']],
+        1: [],
+        2: [['10:00', '12:00'], ['16:00', '19:00']],
+        3: [['13:00', '18:00']],
+        4: [['08:00', '11:00'], ['12:00', '13:00'], ['18:00', '20:00']],
+        5: [],
+        6: [['09:00', '12:00']]
+    }
+}
+const maxNumMembers = 6;
+
+// the row which holds all group entries
+const $requestsRow = $("#requests-container").find(".row");
+
+// Set user's group (top of the page) to group.
+// If group is null, remove the group container from the page.
+function setUserGroup(group) {
+    if (!group) {
+        $("#group-container").remove();
+    } else {
+        $("#group-name").text(group.name);
+        const $numMembersContainer = $("#number-members-container");
+        // adding all filled-in icons
+        let i;
+        for (i = 0; i < group.numMembers; i++) {
+            let $icon = $("<img>", {class: "big-user-icon",
+                                     src:"content/person_filled.png"});
+            $numMembersContainer.append($icon);
+        }
+        // adding all unfilled icons
+        for (; i < maxNumMembers; i++) {
+            const $icon = $("<img>", {class: "big-user-icon",
+                                      src: "content/person_unfilled.png"});
+            $numMembersContainer.append($icon);
+        }
+        // Add the member list. Also put a crown next to the owner and make the user blue.
+        const membersList = group.members.map(member => {
+            const crown = member == owner ? `<img class='member_crown' src='content/crown.png'>` : ``;
+            return member == user ? `<li class='member_you'> ${member} ${crown} </li>` : `<li> ${member} ${crown} </li>`;
+        });
+        // Attach the member list.
+        $("#group-members-container").find("ul").append(membersList.join(""));
+        // Attach the requirement list.
+        const requirementList = group.requirements.map(requirement => `<li>${requirement}</li>`);
+        $("#posting_requirements").append(requirementList.join(""));
+    }
+}
+
+// Add request to "Requests" section of page.
+// "group" is an object with keys name and numMembers.
+function addRequest(group) {
+    const $col = $("<div>", {class: "col-md-4"});
+    const $container = $("<div>", {class: "requests-entry group-entry group-entry-hover card"});
+    const $title = $("<h5>").text(group.name);
+    const $numMembersContainer = $("<div>", {class: "num-group-members-container"});
+
+    // adding all filled-in icons
+    let i;
+    for (i = 0; i < group.numMembers; i++) {
+        const $icon = $("<img>", {class: "small-user-icon",
+                                  src: "content/person_filled.png"});
+        $numMembersContainer.append($icon);
+    }
+    // adding all unfilled icons
+    for (; i < maxNumMembers; i++) {
+        const $icon = $("<img>", {class: "small-user-icon",
+                                  src: "content/person_unfilled.png"});
+        $numMembersContainer.append($icon);
+    }
+
+    $container.append($title);
+    $container.append($numMembersContainer);
+    $col.append($container);
+    $requestsRow.append($col);
+    const url = "group_page.html?gid=" + group.groupId;
+    $container.click(event => { window.location.href=url; });
+}
+
+function addAvailability(group)
+{
+    // add schedule to the page
+    $("#schedule").dayScheduleSelector({
+        startTime: '00:00',
+        endTime: '24:00',
+        interval: 60
+    });
+    $("#schedule").data('artsy.dayScheduleSelector').deserialize(group.availability);
+}
+
+// Populate page with information on page load
+$(document).ready(function() {
+    setUserGroup(userGroup);
+    for (let i = 0; i < groupRequests.length; i++) {
+        addRequest(groupRequests[i]);
+    }
+    addAvailability(userGroup);
+});
