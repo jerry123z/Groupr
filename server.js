d11fd3c6402e6f13cda165c8ae162cbe54e1ed22
Andriy <andriydzikh@gmail.com>
2018-11-30 23:52:19 -0500

    Created getters for the database.

--- server.js
+++ server.js
@@ -6,11 +6,9 @@ const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 const mongoose = require('mongoose');
 
-// Connect to mongo database.
-mongoose.connect('mongodb://localhost:27017/Groupr', { useNewUrlParser: true});
-
 // Import the models.
 const { User, School, Course, Assignment, Group } = require('./models.js');
+const db_api = require('./db_api.js');
 
 // Start the database.
 const app = express();
@@ -18,6 +16,46 @@ app.use(bodyParser.json());
 
 // Start the front end.
 app.use(express.static(__dirname + "/frontend"));
+
+app.get("/user/:id", (req, res) => {
+    db_api.getUser(req.params.id).then(user => {
+        res.send(user);
+    }).catch(error => {
+        res.status(400).send(error);
+    });
+});
+
+app.get("/school/:id", (req, res) => {
+    db_api.getSchool(req.params.id).then(user => {
+        res.send(user);
+    }).catch(error => {
+        res.status(400).send(error);
+    });
+});
+
+app.get("/course/:id", (req, res) => {
+    db_api.getCourse(req.params.id).then(user => {
+        res.send(user);
+    }).catch(error => {
+        res.status(400).send(error);
+    });
+});
+
+app.get("/assignment/:id", (req, res) => {
+    db_api.getAssignment(req.params.id).then(user => {
+        res.send(user);
+    }).catch(error => {
+        res.status(400).send(error);
+    });
+});
+
+app.get("/group/:id", (req, res) => {
+    db_api.getGroup(req.params.id).then(user => {
+        res.send(user);
+    }).catch(error => {
+        res.status(400).send(error);
+    });
+});
 
 app.listen(port, () => {
     console.log("Listening on port " + port);
