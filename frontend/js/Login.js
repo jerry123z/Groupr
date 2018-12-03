75ce6c666351c5c563894724f32968c8205df60b
jerry123z <jerry.zheng@hotmail.ca>
2018-12-03 11:38:28 -0500

    Updated Login page
    Created a form to register new users.
    Changed the back ground

--- frontend/js/Login.js
+++ frontend/js/Login.js
@@ -1,11 +1,17 @@
 $('document').ready(function () {
+    console.log($.urlParam('register'))
+    if ($.urlParam('register')){
+        document.getElementById("login-panel").style.display = "none";
+    } else {
+        document.getElementById("register-panel").style.display = "none";
+    }
 });
 
 $('#login-form').submit(function (e) {
     e.preventDefault();
     //Check fields
-    if ($('#Username').val().length == 0) {
-        throw "Username is empty";
+    if ($('#Email').val().length == 0) {
+        throw "Email is empty";
     }
     else if ($('#Password').val().length == 0) {
         throw "Password is empty";
@@ -20,3 +26,22 @@ $('#login-form').submit(function (e) {
     }
 
 });
+
+$('#register-form').submit(function (e) {
+    e.preventDefault();
+    //Check fields
+    if ($('#EmailReg').val().length == 0) {
+        throw "Email is empty";
+    }
+    else if ($('#PasswordReg').val().length == 0) {
+        throw "Password is empty";
+    }
+});
+
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null) {
+       return null;
+    }
+    return decodeURI(results[1]) || 0;
+}
