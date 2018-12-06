f3372e29295a735ff292abc3e7c4536638369be8
fowlerbr <brennan.fowler@mail.utoronto.ca>
2018-12-05 19:48:02 -0500

    fixed errors caused by merge

--- group.js
+++ group.js
@@ -75,6 +75,17 @@ router.post("/:assignment_id", (req, res) => {
     })
 });
 
+// Route for getting groups for a specific assignment
+router.get("/assignment/:assignment_id", (req, res) => {
+    let assignment_id = req.params.assignment_id;
+
+    dbGet.getGroupsForAssignment(assignment_id).then(groups => {
+        res.send(groups);
+    }).catch(error => {
+        res.status(400).send(error);
+    });
+});
+
 // Create merge request.
 router.post("/merge/:mergeRequestor/:mergeTarget", (req, res) => {
     let userId;
